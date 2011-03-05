5c0a460133da20ed82d38988e836de4c96763089
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-03-05 11:23:02 +0900

    initial import

---
+++ lib/Amon2/Plugin/Web/MobileAgent.pm
@@ -0,0 +1,39 @@
+package Amon2::Plugin::Web::MobileAgent;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Amon2::Plugin::Web::MobileAgent -
+
+=head1 SYNOPSIS
+
+  use Amon2::Plugin::Web::MobileAgent;
+
+=head1 DESCRIPTION
+
+Amon2::Plugin::Web::MobileAgent is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
